8adee8d6864c6bf0c7606351b2eb1b8f475b264f
Danish-2709 <127020341+Danish-2709@users.noreply.github.com>
2023-12-04 16:44:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2411,7 +2411,7 @@ app.get('/api/GetDrPatientData/:RFIID', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
